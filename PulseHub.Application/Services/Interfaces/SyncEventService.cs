e2adfad07849a3a4d679d5b850c76114c849af81
Victor-MacielMF <joaovictormacieldefreitas@gmail.com>
2025-06-19 15:52:40 -0300

    chore: add folder structure for Services with Interfaces and Implementations

---
+++ PulseHub.Application/Services/Interfaces/SyncEventService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulseHub.Application.Services.Interfaces
+{
+    internal class SyncEventService
+    {
+    }
+}
