f086c858408dece7118c0e55f9b922ac62f8eead
Victor-MacielMF <joaovictormacieldefreitas@gmail.com>
2025-06-19 10:36:42 -0300

    test: create ProductRepository integration test with DbContext factory and IDisposable cleanup

--- PulseHub.Infrastructure.Test/TestDbContextFactory.cs
+++ PulseHub.Infrastructure.Test/TestDbContextFactory.cs
@@ -1,6 +1,21 @@
-﻿namespace PulseHub.Infrastructure.Test
+﻿using Microsoft.EntityFrameworkCore;
+using PulseHub.Infrastructure.Data;
+using System;
+
+namespace PulseHub.Infrastructure.Test
 {
-    public class TestDbContextFactory
+    public static class TestDbContextFactory
     {
+        public static PulseHubDbContext CreateDbContext()
+        {
+            var options = new DbContextOptionsBuilder<PulseHubDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new PulseHubDbContext(options);
+            context.Database.EnsureCreated();
+
+            return context;
+        }
     }
 }
